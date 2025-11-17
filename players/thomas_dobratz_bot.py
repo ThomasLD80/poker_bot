21fdb972273dffb4f6d82b0a6b437d5fe8eb3700
Thomas Dobratz <tdobratz8149@student.pps.net>
2025-11-17 10:03:52 -0800

    Starting project

---
+++ players/thomas_dobratz_bot.py
@@ -0,0 +1,10 @@
+"""
+Thomas Dobratz Bot - We shall see how it plays
+This is the bot I am entering in the tournament
+"""
+from typing import List, Dict, Any
+import random
+
+from bot_api import PokerBotAPI, PlayerAction
+from engine.cards import Card
+from engine.poker_game import GameState
